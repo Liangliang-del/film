cc76e0b07a4ca36f05fad68cfb119b6bc23b421d
u <u@LAPTOP-GTAJ67L0>
2019-09-25 23:19:09 +0800

    电影的爬取

--- src/test/java/com/ll/test/TestJsoupFilm.java
+++ src/test/java/com/ll/test/TestJsoupFilm.java
@@ -2,6 +2,7 @@ package com.ll.test;
 
 import java.io.File;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -42,8 +43,11 @@ public class TestJsoupFilm {
 		Film film = null;
 		File input = null;
 		Document doc = null;
-		for (int i = 1; i < 17; i++) {
-			input = new File("D://cinema//"+i+".html");
+		for (int i = 2; i < 22; i++) {
+			if(i==3 || i==9 || i==11 || i==15 || i==17 || i==18 || i==19){
+				continue;
+			}
+			input = new File("D://jsoup//film//"+i+".html");
 			// 解析网页 得到文档对象
 			doc = Jsoup.parse(input, "UTF-8");
 			// 获取指定的<a>
@@ -57,36 +61,51 @@ public class TestJsoupFilm {
 				return;
 			}
 			String name = names.get(0).text();
-			String type = doc.select(".movie-brief-container ul .ellipsis").get(0).text().split(",")[1];
+			String[] types = doc.select(".movie-brief-container ul .ellipsis").get(0).text().split(",");
+			String type = "";
+			if(types.length>1){
+				type = types[1];
+			}
 			String time = doc.select(".movie-brief-container ul .ellipsis").get(1).text().split("/")[1];
 			String pdate = doc.select(".movie-brief-container ul .ellipsis").get(2).text().substring(0, 10);
-			String desc = doc.select(".module .mod-content .dra").get(0).text();
-			Elements servers = doc.select(".mod-content .cinema-main .cinema-brief-container .feature");
+			String desc = doc.select(".main-content .module").get(0).select(".dra").get(0).text();
+			String director = doc.select(".main-content .module").get(1).select(".info").get(0).child(0).text();
+			Elements actors = doc.select(".main-content .module").get(1).select(".info");
 			StringBuffer sb = new StringBuffer();
-			if(servers != null && servers.size()>0){
-				for (int j = 0; j < servers.size(); j++) {
-					Element c1 = servers.get(j).child(0);
-					Element c2 = servers.get(j).child(1);
+			if(actors != null && actors.size()>0){
+				for (int j = 1; j < actors.size(); j++) {
+					Element c1 = actors.get(j).child(0);
+					Element c2 = actors.get(j).select(".role").get(0);
 					if(c1 != null && c2 != null){
 						String pre = c1.text();
 						String ne = c2.text();
-						sb.append(pre).append(":").append(ne).append(";");
+						sb.append(pre).append(";").append(ne).append(";");
 					}
 				}
 			}
 
-			String picsrc = doc.select(".cinema-left .avatar-shadow img").get(0).attr("src");
+			String picsrc = doc.select(".banner .celeInfo-left .avatar-shadow img").get(0).attr("src");
 			System.out.println("picsrc:" + picsrc);
-			String path = "filmImage/cinema/" + new Date().getTime() + "_" + i + ".jpg";
+			String path = "filmImage/film/" + new Date().getTime() + "_" + i + ".jpg";
 			String dest = "D://Tomcat//apache-tomcat-8.0.53//webapps//"+path;
-			PicUtil.copyPic("D://cinema//"+picsrc, desc);
+			PicUtil.copyPic("D://jsoup//film//"+picsrc, dest);
 
-			System.out.println("cinema:" + film);
+			film.setfName(name);
+			film.setfDirector(director);
+			film.setfActor(sb.toString());
+			film.setfPdate(new SimpleDateFormat( "yyyy-MM-dd" ).parse(pdate));
+			film.setfType(type);
+			film.setfTime(time);
+			film.setfPic(path);
+			film.setfStatus(1);
+			film.setfDesc(desc);
+			
+			System.out.println("film:" + film);
 			list.add(film);
 
 //			pictureResponse.close(); // pictureResponse关闭
 			
-			Thread.sleep(5000);
+//			Thread.sleep(3000);
 		}
 
 		for (Film c : list) {
